9f904517b73d3e3a74a814b7a6451abf2b69f4b0
GreenBean0303 <lootsmannagnes@gmail.com>
2025-04-01 13:25:49 +0300

    Ülesanne 3 - andmete pärimine Meals.js komponendis

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import Header from "./components/Header"
+import Header from "./components/Header";
+import Meals from "./components/Meals";
+
 const App = () => {
- return (
-    <Header>
-     <h1>Food Order App</h1>
-     
-    </Header>
+  return (
+    <>
+      <Header>
+        <h1>Food Order App</h1>
+      </Header>
+
+      <main>
+        <Meals />
+      </main>
+    </>
   );
-}
+};
 
 export default App;
--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,11 +1,37 @@
-const Meals = () => {
-    return (
-        <ul id="meals">
-            { 
-                // list of meals
-            }
-        </ul>
-    )
-}
+import { useEffect, useState } from "react";
 
-export default Meals
+const Meals = () => {
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      try {
+        const response = await fetch("http://localhost:3001/meals");
+        const data = await response.json();
+        console.log("meals:",  data);
+        setMeals(data);
+      } catch (error) {
+        console.error("Fetch error:", error);
+      }
+    };
+  
+    fetchMeals();
+  }, []);
+  
+
+  return (
+    <ul id="meals">
+      {meals.map((meal) => (
+        <li key={meal.id}>
+          <h3>{meal.name}</h3>
+          <p>{meal.description}</p>
+          <p>Hind: {meal.price} €</p>
+          <img src={meal.image} alt={meal.name} width="200" />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+
+export default Meals;
